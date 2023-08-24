606c0ee72d0f8fdce7fe49ddd2a70c5373855ed1
annavancity <anna.designers@yahoo.com>
2023-08-24 13:44:40 -0700

    code updates

--- src/DragDropTable.js
+++ src/DragDropTable.js
@@ -1,176 +1,166 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import {
   Button,
   Checkbox,
-  FormControlLabel, Paper,
+  FormControlLabel,
+  Paper,
   Table,
-  TableBody, TableCell,
+  TableBody,
+  TableCell,
   TableContainer,
   TableHead,
   TableRow,
-  TextField
-} from '@mui/material';
-import DragableRow from './DragableRow';
-import {DndProvider} from 'react-dnd';
-import {HTML5Backend} from 'react-dnd-html5-backend';
+  TextField,
+} from "@mui/material";
+import DragableRow from "./DragableRow";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 const DragDropTable = () => {
-
-  const initialColumns = ['Row Name', 'Data'];
+  const initialColumns = ["Row Name", "Data"];
   const initialData = [
-    {'Row Name': 'Row 1', Data: 'Data 1', editable: false},
-    {'Row Name': 'Row 2', Data: 'Data 2', editable: false},
-    {'Row Name': 'Row 3', Data: 'Data 3', editable: false}
-  ]
+    { "Row Name": "Row 1", Data: "Data 1", editable: false },
+    { "Row Name": "Row 2", Data: "Data 2", editable: false },
+    { "Row Name": "Row 3", Data: "Data 3", editable: false },
+  ];
   const [columns, setColumns] = useState(initialColumns);
   const [tableData, setTableData] = useState(initialData);
   const [newRow, setNewRow] = useState({});
   const [showAddRowForms, setShowAddRowForms] = useState(false);
-  const [newColumn, setNewColumn] = useState('');
+  const [newColumn, setNewColumn] = useState("");
   const [showColumnInputs, setShowColumnInputs] = useState(false);
 
   //checkbox
 
   const handleCheckboxChange = (event) => {
     const columnName = event.target.name;
-    if(event.target.checked) {
+    if (event.target.checked) {
       setColumns([...columns, columnName]);
     } else {
-      setColumns(columns.filter((col) => col !== columnName))
+      setColumns(columns.filter((col) => col !== columnName));
     }
-  }
+  };
 
   const handleNewColumnChange = (event) => {
     setNewColumn(event.target.value);
-  }
+  };
 
   const handleAddColumn = () => {
-    if (newColumn.trim() !== '') {
+    if (newColumn.trim() !== "") {
       setColumns([...columns, newColumn]);
-      setNewColumn('');
+      setNewColumn("");
     }
-  }
+  };
 
   const handleNewRowChange = (event) => {
-    setNewRow({...newRow, [event.target.name]: event.target.value});
-  }
+    setNewRow({ ...newRow, [event.target.name]: event.target.value });
+  };
 
   const handleAddRow = () => {
-    setTableData([...tableData, {...newRow, editable: false}]);
+    setTableData([...tableData, { ...newRow, editable: false }]);
     setNewRow({});
     setShowAddRowForms(false);
-  }
+  };
 
   const handleEditRow = (index, col, value) => {
     const newData = [...tableData];
     newData[index][col] = value;
     setTableData(newData);
-  }
+  };
 
   //drag and drop function
   const handleMoveRow = (fromIndex, toIndex) => {
     const newData = [...tableData];
     newData.splice(toIndex, 0, newData.splice(fromIndex, 1)[0]);
     setTableData(newData);
-  }
+  };
 
   const handleToggleEditRow = (index) => {
-    if(tableData[index]. editable) {
+    if (tableData[index].editable) {
       const newData = [...tableData];
       newData[index].editable = false;
       setTableData(newData);
     } else {
       const newData = tableData.map((row, i) =>
-        i === index ? {...row, editable: true} : row
+        i === index ? { ...row, editable: true } : row
       );
-      setTableData(newData)
+      setTableData(newData);
     }
-  }
+  };
 
   return (
     <DndProvider backend={HTML5Backend}>
       <div>
-
-          <Button onClick={() => setShowColumnInputs(!showColumnInputs)}>Add Column</Button>
-          {showColumnInputs && (
-<div>
-
-          <TextField
-          label="New Column"
-          value={newColumn}
-          onChange={handleNewColumnChange}
-          />
-
-          <Button onClick={handleAddColumn}>Add</Button>
+        <Button onClick={() => setShowColumnInputs(!showColumnInputs)}>
+          Add Column
+        </Button>
+        {showColumnInputs && (
           <div>
-            {columns.map((col, idx) => (
-
-
-
-            <FormControlLabel
-              key={idx}
-            control={
-              <Checkbox
-                checked={columns.includes(col)}
-                onChange={handleCheckboxChange}
-                name={col}
-              />
-            }
-            label={col}
+            <TextField
+              label="New Column"
+              value={newColumn}
+              onChange={handleNewColumnChange}
             />
-            ))}
+            <Button onClick={handleAddColumn}>Add</Button>
+            <div>
+              {columns.map((col, idx) => (
+                <FormControlLabel
+                  key={idx}
+                  control={
+                    <Checkbox
+                      checked={columns.includes(col)}
+                      onChange={handleCheckboxChange}
+                      name={col}
+                    />
+                  }
+                  label={col}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-          )}
-
-
-          <Button onClick={() => setShowAddRowForms(!showAddRowForms)}>Add Row</Button>
+        )}
+        <Button onClick={() => setShowAddRowForms(!showAddRowForms)}>
+          Add Row
+        </Button>
         {showAddRowForms && (
           <div>
-          {columns.map((col,idx) => (
-
-            <TextField
-              key={idx}
-              label={col}
-              name={col}
-              value={newRow[col] || ''}
-              onChange={handleNewRowChange}
-            />
-
-          ))}
+            {columns.map((col, idx) => (
+              <TextField
+                key={idx}
+                label={col}
+                name={col}
+                value={newRow[col] || ""}
+                onChange={handleNewRowChange}
+              />
+            ))}
             <Button onClick={handleAddRow}>Add Row</Button>
           </div>
         )}
-
       </div>
 
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
-
             <TableRow>
               {columns.map((col, idx) => (
                 <TableCell key={idx}>{col}</TableCell>
-              ) )}
+              ))}
             </TableRow>
-
           </TableHead>
           <TableBody>
             {tableData.map((row, index) => (
-              < DragableRow
-              key={index}
-              row={row}
-              index={index}
-              columns={columns}
-              handleEditRow={handleEditRow}
-              handleMoveRow={handleMoveRow}
-              handleToggleEditRow={handleToggleEditRow}
+              <DragableRow
+                key={index}
+                row={row}
+                index={index}
+                columns={columns}
+                handleEditRow={handleEditRow}
+                handleMoveRow={handleMoveRow}
+                handleToggleEditRow={handleToggleEditRow}
               />
-
             ))}
           </TableBody>
         </Table>
-
       </TableContainer>
     </DndProvider>
   );
--- src/DragableRow.js
+++ src/DragableRow.js
@@ -1,75 +1,88 @@
-import React, {useState} from 'react';
-import {useDrag, useDrop} from 'react-dnd';
-import {Button, IconButton, TableCell, TableRow, TextField} from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
+import React, { useState } from "react";
+import { useDrag, useDrop } from "react-dnd";
+import {
+  Button,
+  IconButton,
+  TableCell,
+  TableRow,
+  TextField,
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
 
-const ItemType = 'Table_Row';
-const DragableRow = ({index, row, columns, handleEditRow, handleMoveRow, handleToggleEditRow}) => {
-  const [,ref] = useDrag({
+const ItemType = "Table_Row";
+const DragableRow = ({
+                       index,
+                       row,
+                       columns,
+                       handleEditRow,
+                       handleMoveRow,
+                       handleToggleEditRow,
+                     }) => {
+  const [, ref] = useDrag({
     type: ItemType,
-    item: {index}
-  })
+    item: { index },
+  });
 
   const [, drop] = useDrop({
     accept: ItemType,
     hover: (draggedItem) => {
-      if(draggedItem.index !== index) {
+      if (draggedItem.index !== index) {
         handleMoveRow(draggedItem.index, index);
-        draggedItem.index = index
+        draggedItem.index = index;
       }
-    }
-  })
+    },
+  });
 
-  const [cellStates, setCellStates] = useState(columns.reduce((acc, col)=>({...acc, [col] : row[col]}), {}))
+  const [cellStates, setCellStates] = useState(
+    columns.reduce((acc, col) => ({ ...acc, [col]: row[col] }), {})
+  );
   const handleToggleEdit = () => {
-    handleToggleEditRow(index)
-  }
+    handleToggleEditRow(index);
+  };
 
   const handleSaveRow = () => {
     Object.entries(cellStates).forEach(([col, value]) => {
       handleEditRow(index, col, value);
     });
     handleToggleEditRow(index);
-  }
+  };
 
   const handleCancelEdit = () => {
-    setCellStates(columns.reduce((acc, col) => ( {...acc, [col]: row[col] }) , {}));
-    handleToggleEditRow(index)
-  }
-
-
+    setCellStates(
+      columns.reduce((acc, col) => ({ ...acc, [col]: row[col] }), {})
+    );
+    handleToggleEditRow(index);
+  };
 
   return (
     <TableRow ref={(node) => ref(drop(node))}>
-      {columns.map((col, idx) =>(
-      <TableCell key={idx}>
-        {row.editable ? (
-          <TextField
-            value={cellStates[col]}
-            onChange = {(e) => setCellStates({...cellStates, [col]: e.target.value})}
-          />
-        ) : (
+      {columns.map((col, idx) => (
+        <TableCell key={idx}>
+          {row.editable ? (
+            <TextField
+              value={cellStates[col]}
+              onChange={(e) =>
+                setCellStates({ ...cellStates, [col]: e.target.value })
+              }
+            />
+          ) : (
             row[col]
-          )
-        }
-      </TableCell>
+          )}
+        </TableCell>
       ))}
       <TableCell>
-
-        {row.editable ?
-            (
+        {row.editable ? (
           <>
-          <Button onClick={handleSaveRow}>Save</Button>
-          <Button onClick={handleCancelEdit}>Cancel</Button>
+            <Button onClick={handleSaveRow}>Save</Button>
+            <Button onClick={handleCancelEdit}>Cancel</Button>
           </>
-          ) : (
-            <IconButton onClick={handleToggleEdit} area-label='Edit'>
-              <EditIcon />
-            </IconButton>
-          )}
+        ) : (
+          <IconButton onClick={handleToggleEdit} area-label="Edit">
+            <EditIcon />
+          </IconButton>
+        )}
       </TableCell>
     </TableRow>
-
   );
 };
 
